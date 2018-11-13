62a5701a07dfe94875a795aa21d0004ee1b52b9d
1uar11 <raularispeveizaga@gmail.com>
2018-11-13 17:49:33 -0400

    practica1.4

---
+++ src/main/java/Programador.java
@@ -0,0 +1,18 @@
+public class Programador extends Empleado{
+    int lineasDeCodigoPorHora;
+    String lenguajeDominante;
+
+    public  Programador(String nombre, String cedula, int edad, boolean casado, double salario, int lineasDeCodigoPorHora, String lenguajeDominante){
+
+        super.nombre = nombre;
+        super.cedula = cedula;
+        super.edad = edad;
+        super.casado = casado;
+        super.salario = salario;
+        this.lineasDeCodigoPorHora = lineasDeCodigoPorHora;
+        this.lenguajeDominante = lenguajeDominante;
+    }
+
+    public Programador(){
+    }
+}
